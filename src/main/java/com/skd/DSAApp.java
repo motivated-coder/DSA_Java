63124123bc9ea981bc3d8d521dd1e368e505b91a
motivated-coder <sumeetdwivedi11@outlook.com>
2023-07-15 18:31:41 +0530

    added getter and setter and print methods

--- src/main/java/com/skd/DSAApp.java
+++ src/main/java/com/skd/DSAApp.java
@@ -5,6 +5,8 @@ import com.skd.linkedlist.LinkedListProxy;
 public class DSAApp {
     public static void main(String[] args) {
         LinkedListProxy linkedListProxy = new LinkedListProxy(2);
+        linkedListProxy.append(4);
+        linkedListProxy.printList();
 
     }
 }
