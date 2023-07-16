c8cac36880e6e5a00f112974cb8660b24d21ddd7
motivated-coder <sumeetdwivedi11@outlook.com>
2023-07-16 18:26:21 +0530

    added findMiddleNode method for LinkedListProxy

--- src/main/java/com/skd/linkedlist/LinkedListProxy.java
+++ src/main/java/com/skd/linkedlist/LinkedListProxy.java
@@ -162,6 +162,16 @@ public class LinkedListProxy {
         }
     }
 
+    public Node findMiddleNode(){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
     public Node getHead() {
         return head;
     }
