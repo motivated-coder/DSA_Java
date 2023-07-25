cb44cce68f402817e222b948f40891a47bbd395a
motivated-coder <sumeetdwivedi11@outlook.com>
2023-07-25 23:52:09 +0530

    added prepend method in DoubleLinkedListProxy

--- src/main/java/com/skd/doublelinkedlist/DoubleLinkedListProxy.java
+++ src/main/java/com/skd/doublelinkedlist/DoubleLinkedListProxy.java
@@ -55,4 +55,18 @@ public class DoubleLinkedListProxy {
         length--;
         return temp;
     }
+
+    public void prepend(int value){
+        Node node = new Node(value);
+        if(length == 0){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next=head;
+            head.prev = node;
+            head=node;
+        }
+        length++;
+    }
 }
